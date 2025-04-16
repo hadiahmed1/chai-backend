a6c25382c1f9e4b1eedd6c127116c1c4535c8b56
Hadi <969hadiahmed@gmail.com>
2025-04-16 14:16:53 +0530

    feat: configured express app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "dotenv/config"
 import connectDB from "./db/connectDB.js";
-connectDB();
+import app from "./app.js";
+connectDB()
+    .then(() => {
+        const port=process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`)
+        })
+    })
+    .catch(error=>console.log("ERROR:>>",  error));
