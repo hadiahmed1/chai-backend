68c99dc384057248a6c4ae48f7257f60ac72c2e8
Hadi <969hadiahmed@gmail.com>
2025-04-17 15:34:18 +0530

    feat: auth middleware to check jwt

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!accessToken) throw new ApiError(401, "Unauthorized");
+
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET = ABC123);
+
+        const user = User.findById(decodedToken?._id);
+        if (!user) throw new ApiError(401, "Unauthorized");
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Token")
+    }
+})
