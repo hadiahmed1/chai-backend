71c08d9ffcd1d7890a418a52388c1338676b4ebe
Hadi <969hadiahmed@gmail.com>
2025-04-17 14:50:32 +0530

    feat: login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,19 @@ import ApiResponse from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 
+const generateAccessAndRefreshToken = (user) => {
+    try {
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateAccessToken();
+        user.refreshToken = refreshToken;
+        user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "Couldn't generate tokens");
+    }
+}
+
 export const registerUser = asyncHandler(async (req, res) => {
     //getting user details
     const { username, email, fullName, password } = req.body;
@@ -17,13 +30,13 @@ export const registerUser = asyncHandler(async (req, res) => {
     //files
     const avatarfilePath = req.files?.avatar[0]?.path;
     let coverImagePath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0)
-    coverImagePath = req.files?.coverImage[0]?.path;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+        coverImagePath = req.files?.coverImage[0]?.path;
     if (!avatarfilePath) throw new ApiError(400, "Avatar image required");
 
     const avatar = await uploadOnCloudinary(avatarfilePath);
     const coverImage = (coverImagePath) ? await uploadOnCloudinary(coverImagePath) : null;
-    if (!avatar) throw new ApiError("401", null,"Couldnt upload");
+    if (!avatar) throw new ApiError("401", null, "Couldnt upload");
 
     const user = new User({
         username, email, fullName, password,
@@ -34,3 +47,28 @@ export const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json(new ApiResponse(200, user, "User created successfully"))
 
 });
+
+export const loginUser = asyncHandler(async (req, res) => {
+    //get email and password
+    const { email, username, password } = req.body;
+    if (!username && !password) throw new ApiError(400, "Please enter username or password");
+    //get corresponding user
+    const user = await User.findOne({ $or: [{ email }, { username }] }).select("password");
+    if (!user) throw new ApiError(404, "User not found");
+    console.log("user:>>", user);
+    //match password
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+    if (!isPasswordCorrect) throw new ApiError(401, "Inavalid credentials");
+    //generate and return access and refreshToken (COOKIES)
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    const { accessToken, refreshToken } = generateAccessAndRefreshToken(user);
+    // res.status(200).json(new ApiResponse(200, generateAccessAndRefreshToken(user), "loggin successfull"));
+    return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(200, { user, accessToken, refreshToken }, "User loggin successfull"));
+
+})
