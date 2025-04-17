96332bda46a97d19e6853c0e15feeedcfe53fddc
Hadi <969hadiahmed@gmail.com>
2025-04-17 12:39:54 +0530

    fix: asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,8 @@ const asyncHandler = (fn) => async (req, res, next) => {
     try {
         await fn(req, res, next)
     } catch (error) {
-        res.status(error.code || 500).json({
+        console.log(error);
+        res.status(error.statusCode || 500).json({
             success: false,
             message: error.message
         })
